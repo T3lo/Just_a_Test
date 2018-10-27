073357911f12b5b5a55ea8b9029f50f1c64b4948
T3lo <ritwik97bhattacharya@gmail.com>
2018-10-27 20:01:59 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Just a Test
+Sat Oct 27 18:50:55 IST 2018
+Sat Oct 27 18:52:34 IST 2018
---
+++ github_push.sh
@@ -0,0 +1,7 @@
+date >> README.md
+git init
+git add README.md
+git commit -m $1
+git remote add origin https://github.com/T3lo/Just_a_Test.git
+git push -u origin master
+
